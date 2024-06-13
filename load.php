200e760042840a1f6f19790d67fe6e002f55d875
mtmsujan <mtmsujon@gmail.com>
2024-06-13 16:27:21 +0600

    file uploaded

---
+++ load.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * File loader file
+ * all files in this folder will be loaded
+ */
+
+defined( "ABSPATH" ) || exit( "Direct Access Not Allowed" );
+
+require_once BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/inc/files/file-db-table-create.php';
+require_once BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/inc/files/file-import-products-woo.php';
+require_once BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/inc/files/file-insert-products-db.php';
+require_once BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/inc/files/file-api-endpoints.php';
